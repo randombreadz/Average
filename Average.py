313f45a5c146aa98345156c9e55d0524d6377501
randombreadz <83088858+randombreadz@users.noreply.github.com>
2022-05-19 19:30:25 -0700

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,58 @@
+from numpy import average
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv("newdata.csv")
+data = df["average"].tolist()
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index = random.randint(0, len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+def show_fig(mean_list):
+    df = mean_list
+    mean = statistics.mean(df)
+    fig = ff.create_distplot([df],["average"],show_hist = True)
+    fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 1], mode="lines", name="MEAN"))
+    fig.show()
+
+def setup():
+    mean_list = []
+    for i in range(0, 1000):
+        set_of_means = random_set_of_mean(100)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+
+    mean = statistics.mean(mean_list)
+    print("Mean of sampling distrubion -", mean)
+
+setup()
+
+average_mean = statistics.mean(data)
+print("Population mean -", average_mean)
+
+def standard_deviation():
+    mean_list = []
+    for i in range(0, 1000):
+        set_of_means = random_set_of_mean(100)
+        mean_list.append(set_of_means)
+        
+    std_deviation = statistics.stdev(mean_list)
+    print("Standard deviation of sampling distrubion -", std_deviation)
+
+standard_deviation()
+
+
+
+
+
+
